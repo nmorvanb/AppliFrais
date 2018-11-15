3f5c9183dd6ee3d660d758d509a75b3d13ec1c16
maeltanneau <tanneaumael@gmail.com>
2018-11-15 14:01:18 +0100

    s

--- application/views/v_visVoirListeFrais.php
+++ application/views/v_visVoirListeFrais.php
@@ -30,7 +30,7 @@
 
 
 	<table class="listeLegere">
-		<caption>Descriiptif des éléments hors forfait</caption>
+		<caption>Descriptif des éléments hors forfait</caption>
 		<tr>
 			<th >Date</th>
 			<th >Libellé</th>
