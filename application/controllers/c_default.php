d65ba3abca87d664a90c4cf1fc6c05babb85d671
nmorvanb <nicolas.morvan.benot@gmail.com>
2018-11-22 16:06:58 +0100

    Envoie du metier a la fonction connecter

--- application/controllers/c_default.php
+++ application/controllers/c_default.php
@@ -5,21 +5,21 @@
  * Si aucune spécification de contrôleur n'est précisée dans l'URL du navigateur
  * c'est le contrôleur par défaut qui sera invoqué. Son rôle est :
  * 		+ d'orienter vers le bon contrôleur selon la situation
- * 		+ de traiter le retour du formulaire de connexion 
+ * 		+ de traiter le retour du formulaire de connexion
 */
 class C_default extends CI_Controller {
 
 	/**
-	 * Fonctionnalité par défaut du contrôleur. 
+	 * Fonctionnalité par défaut du contrôleur.
 	 * Vérifie l'existence d'une connexion :
-	 * Soit elle existe et on redirige vers le contrôleur de VISITEUR, 
+	 * Soit elle existe et on redirige vers le contrôleur de VISITEUR,
 	 * soit elle n'existe pas et on envoie la vue de connexion
 	*/
 	public function index()
 	{
 		$this->load->model('authentif');
-		
-		if (!$this->authentif->estConnecte()) 
+
+		if (!$this->authentif->estConnecte())
 		{
 			$data = array();
 			$this->templates->load('t_connexion', 'v_connexion', $data);
@@ -30,19 +30,19 @@ class C_default extends CI_Controller {
 			redirect('/c_visiteur/');
 		}
 	}
-	
+
 	/**
 	 * Traite le retour du formulaire de connexion afin de connecter l'utilisateur
 	 * s'il est reconnu
 	*/
-	public function connecter () 
-	{	// TODO : conrôler que l'obtention des données postées ne rend pas d'erreurs 
+	public function connecter ()
+	{	// TODO : conrôler que l'obtention des données postées ne rend pas d'erreurs
 
 		$this->load->model('authentif');
-		
+
 		$login = $this->input->post('login');
 		$mdp = $this->input->post('mdp');
-		
+
 		$authUser = $this->authentif->authentifier($login, $mdp);
 
 		if(empty($authUser))
@@ -52,9 +52,9 @@ class C_default extends CI_Controller {
 		}
 		else
 		{
-			$this->authentif->connecter($authUser['id'], $authUser['nom'], $authUser['prenom']);
+			$this->authentif->connecter($authUser['id'], $authUser['nom'], $authUser['prenom'], $authUser['metier']);
 			$this->index();
 		}
 	}
-	
+
 }
