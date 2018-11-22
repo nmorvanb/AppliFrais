034ec1370d1bc22352e150364c2d7710903a6044
maeltanneau <tanneaumael@gmail.com>
2018-11-22 13:51:36 +0100

    Enregistrement fiche de frais , lors de la suppression.

--- application/models/a_visiteur.php
+++ application/models/a_visiteur.php
@@ -158,5 +158,6 @@ class A_visiteur extends CI_Model {
 	{	// TODO : s'assurer que les paramètres reçus sont cohérents avec ceux mémorisés en session et cohérents entre eux
 
 	    $this->dataAccess->supprimerLigneHorsForfait($idLigneFrais);
+      $this->dataAccess->recalculeMontantFiche($idVisiteur,$mois);
 	}
 }
