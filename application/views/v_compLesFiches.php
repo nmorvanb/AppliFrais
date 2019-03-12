58ae97cec34654a0f3d89534905bc5edf750f994
jordanbolou <jordanbolou@laposte.net>
2019-03-12 11:30:41 +0100

    modifications vue les fiches

--- application/views/v_compLesFiches.php
+++ application/views/v_compLesFiches.php
@@ -20,17 +20,17 @@
 		<?php
 			foreach( $lesFiches as $uneFiche)
 			{
-				$pdfLink = anchor('c_comptable/imprimeFiche/'.$uneFiche['id'], 'imprimer',  'title="Imprimer la fiche en PDF"');
-				$signeLink = anchor('c_comptable/signeFiche/'.$uneFiche['id'], 'signer',  'title="Signer la fiche"  onclick="return confirm(\'Voulez-vous vraiment signer cette fiche ?\');"');
-				$refuseLink = anchor('c_comptable/signeFiche/'.$uneFiche['id'], 'refuser',  'title="Refuser la fiche"  onclick="return confirm(\'Voulez-vous vraiment refuser cette fiche ?\');"');
+				$pdfLink = anchor('c_comptable/imprimeFiche/'.$uneFiche['idVisiteur'].'/'.$uneFiche['mois'], 'imprimer',  'title="Imprimer la fiche en PDF"');
+				$valideLink = anchor('c_comptable/valideFiche/'.$uneFiche['idVisiteur'].'/'.$uneFiche['mois'], 'valider',  'title="Valider la fiche"  onclick="return confirm(\'Voulez-vous vraiment valider cette fiche ?\');"');
+				$refuseLink = anchor('c_comptable/refuseFiche/'.$uneFiche['idVisiteur'].'/'.$uneFiche['mois'], 'refuser',  'title="Refuser la fiche"  onclick="return confirm(\'Voulez-vous vraiment refuser cette fiche ?\');"');
 
 				echo
 				'<tr>
 					<td class="date">'.anchor('c_comptable/voirFiche/'.$uneFiche['idVisiteur'].'/'.$uneFiche['mois'], $uneFiche['mois'],  'title="Consulter la fiche"').'</td>
 					<td class="montant">'.$uneFiche['montantValide'].'</td>
 					<td class="date">'.$uneFiche['dateModif'].'</td>
-					<td class="action">'.$modLink.'</td>
-					<td class="action">'.$signeLink.'</td>
+					<td class="action">'.$valideLink.'</td>
+					<td class="action">'.$refuseLink.'</td>
 					<td class="action">'.$pdfLink.'</td>
 				</tr>';
 			}
